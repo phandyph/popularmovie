979f85f83e76885f2e2d61b7a3d6a5fc953b1612
phandyph <phornphandy23@gmail.com>
2023-04-04 17:03:07 +0700

    responsive on actionContainer such as sort and filter

--- movie/src/components/MainContainer/index.jsx
+++ movie/src/components/MainContainer/index.jsx
@@ -3,7 +3,7 @@ import ActionContainer from "../ActionContainer";
 import CardContainer from "../CardContainer";
 import "./MainContainer.css";
 import movies from "../../data/Movies.json";
-import ActionButton from "../ActionButton";
+
 const START_INDEX = 0;
 const ITEMS_PER_PAGE = 4;
 
@@ -24,22 +24,17 @@ const MainContainer = () => {
   };
 
   return (
-    <>
-      {/* actionBtn instead of actionCard */}
-      <ActionButton movies={movies} onSearch={onGetMoviesAfterDoAction} />
-
-      <div className="actionAndCardContainer">
-        <ActionContainer movies={movies} onSearch={onGetMoviesAfterDoAction} />
-        <CardContainer
-          itemForCardContainer={{
-            items: movieList.slice(START_INDEX, fourMoviesLength),
-            fourItems: fourMoviesLength,
-            amountItemsDisplay: amountMoviesDisplay,
-          }}
-          onClick={onLoadMore}
-        />
-      </div>
-    </>
+    <div className="actionAndCardContainer">
+      <ActionContainer movies={movies} onSearch={onGetMoviesAfterDoAction} />
+      <CardContainer
+        itemForCardContainer={{
+          items: movieList.slice(START_INDEX, fourMoviesLength),
+          fourItems: fourMoviesLength,
+          amountItemsDisplay: amountMoviesDisplay,
+        }}
+        onClick={onLoadMore}
+      />
+    </div>
   );
 };
 
